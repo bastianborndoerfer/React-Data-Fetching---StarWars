3201016f7a8dc330e4a859e7bd049ffbb7838f91
bastianborndoerfer <bastian.borndoerfer@gmx.de>
2023-07-31 11:50:22 +0200

    add swr-fetching

--- pages/characters/1.js
+++ pages/characters/1.js
@@ -1,17 +1,29 @@
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+const id = 1;
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Character() {
-  const id = 1;
+  const { data, error } = useSWR(`https://swapi.dev/api/people/${id}`, fetcher);
+
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
